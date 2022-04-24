3b71753f27aba1d678c555fe7086389b9e16ed6f
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-04-24 15:23:27 +0900

    create api

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,7 +9,8 @@ class TaskController extends Controller
 {
     public function getAllTasks()
     {
-        // logic to get all Tasks goes here
+        $tasks = Task::get()->toJson(JSON_PRETTY_PRINT);
+        return response($tasks, 200);
     }
 
     public function createTask(Request $request)
@@ -26,16 +27,43 @@ class TaskController extends Controller
 
     public function getTask($id)
     {
-        // logic to get a Task record goes here
+        if (Task::where('id', $id)->exists()) {
+            $task = Task::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($task, 200);
+        }
     }
 
     public function updateTask(Request $request, $id)
     {
-        // logic to update a Task record goes here
+        if (Task::where('id', $id)->exists()) {
+            $task = Task::find($id);
+            $task->title = is_null($request->title) ? $task->title : $request->title;
+            $task->content = is_null($request->content) ? $task->content : $request->content;
+            $task->save();
+
+            return response()->json([
+                "message" => "records updated successfully"
+            ], 200);
+        } else {
+            return response()->json([
+                "message" => "task not found"
+            ], 404);
+        }
     }
 
     public function deleteTask($id)
     {
-        // logic to delete a Task record goes here
+        if (Task::where('id', $id)->exists()) {
+            $task = Task::find($id);
+            $task->delete();
+
+            return response()->json([
+                "message" => "records deleted"
+            ], 202);
+        } else {
+            return response()->json([
+                "message" => "task not found"
+            ], 404);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('tasks', 'TaskController@getAllTasks');
-Route::get('tasks/{id}', 'TaskController@getTask');
-// Route::post('tasks', 'TaskController@createTask');
+Route::get('tasks', [TaskController::class, 'getAllTasks']);
+Route::get('tasks/{id}', [TaskController::class, 'getTask']);
 Route::post('tasks', [TaskController::class, 'createTask']);
-Route::put('tasks/{id}', 'TaskController@updateTask');
-Route::delete('tasks/{id}', 'TaskController@deleteTask');
+Route::put('tasks/{id}', [TaskController::class, 'updateTask']);
+Route::delete('tasks/{id}', [TaskController::class, 'deleteTask']);
